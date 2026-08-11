b941c9f6d24fec9da349b7eb9f96eab269828024
effor <effortimp@gmail.com>
2026-08-11 21:56:05 +0800

    init Project

---
+++ src/main/java/esun/social_media_test/SocialMediaTestApplication.java
@@ -0,0 +1,13 @@
+package esun.social_media_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esun/social_media_test/SocialMediaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package esun.social_media_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
